59fff0ba03b88ae6676b54b8dae265775987dbca
Lonewolfrex <samparkprakash27@gmail.com>
2024-10-09 14:02:13 +0530

    included the session manager

--- contactListApp/contactListApp/settings.py
+++ contactListApp/contactListApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='${{ secrets.DJANGO_SECRET }}'
+SECRET_KEY='django-insecure-t(5vt8evl^jlf%)v_lqpl0rm&v3+moxad((sa*q4na=6jtl7f)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
